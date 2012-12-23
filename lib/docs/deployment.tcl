3244165c040461472bd6dc8992260e8c7e3dbfd3
Tomasz Konojacki <me@xenu.tk>
2012-12-23 21:42:29 +0100

    more documentation

---
+++ lib/docs/deployment.tcl
@@ -0,0 +1,14 @@
+# Class: ::Achatina::Docs::Deployment
+#
+# There are three ways to deploy Achatina application: you can use built-in httpd server,
+# use built-in httpd server through reverse proxy or use CGI.
+#
+# Achatina's CGI interface was tested under IIS8 and Apache 2.4.
+#
+# CGI deployment:
+#
+# 1. You need to hide *.yml files from user
+#
+# 2. Your webserver should treat *.tcl files as CGI scripts
+#
+# 3. In most webservers you need to modify shebang in app.tcl file
